3e6398bc2bc712334835d0cdcc28e65b0cd34c5a
Avantica123 <dattalandge001@gmail.com>
2022-09-05 23:35:42 +0530

    blog validation

--- src/controller.js/blogcontroller.js
+++ src/controller.js/blogcontroller.js
@@ -1,10 +1,17 @@
 const Blogmodel=require('../Model.js/Blogmodel')
+const Authormodel=require('../Model.js/Authormodel')
 const createblog = async (req, res)=>{
 
     try {
         if (Object.keys(req.body).length == 0) {
             return res.status(400).send({ Error: "Body  should be not emety" })
         }
+        let id=req.body.authorId
+        let author=await Authormodel.findById(id)
+        if(!author){
+            return res.status(404).send({status:false,msg:"User not found"})
+        }
+
         let data = new Blogmodel(req.body)
         let result = await data.save()
         res.status(201).send({status:true,data:result})
