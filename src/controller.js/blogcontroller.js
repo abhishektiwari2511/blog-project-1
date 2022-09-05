eedda4ee001845b1fe921e6bd67c2791a61261bf
Avantica123 <dattalandge001@gmail.com>
2022-09-05 17:36:54 +0530

    second commit

---
+++ src/controller.js/blogcontroller.js
@@ -0,0 +1,18 @@
+const Blogmodel=require('../Model.js/Blogmodel')
+const createblog = async (req, res)=>{
+
+    try {
+        if (Object.keys(req.body).length == 0) {
+            return res.status(400).send({ Error: "Body  should be not emety" })
+        }
+        let data = new Blogmodel(req.body)
+        let result = await data.save()
+        res.status(201).send(result)
+    }
+    catch (error) {
+        return res.status(500).send({status:false,msg:error.message})
+    }
+
+
+}
+module.exports.createblog=createblog
