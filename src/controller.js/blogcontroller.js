886a6914db00473f47e1555891d06a4efd8d3ddd
Chanchal160817 <you@example.com>
2022-09-06 15:19:33 +0530

    delete blockid api

--- src/controller.js/blogcontroller.js
+++ src/controller.js/blogcontroller.js
@@ -40,5 +40,32 @@ const blogsUpdate = async (req,res)=> {
     }
 }
 
+const deleted = async function (req, res) {
+    try {
+        //Validate: The blogId is present in request path params or not.
+        let blog_Id = req.params.blogId
+        if (!blog_Id) return res.status(400).send({ status: false, msg: "Blog Id is required" })
+
+        //Validate: The blogId is valid or not.
+        let blog = await Blogmodel.findById(blog_Id)
+        if (!blog) return res.status(404).send({ status: false, msg: "Blog does not exists" })
+
+        //Validate: If the blogId is not deleted (must have isDeleted false)
+        let is_Deleted = blog.isDeleted
+        if (is_Deleted == true) return res.status(404).send({ status: false, msg: "Blog is already deleted" })
+
+        //Delete a blog by changing the its isDeleted to true.
+        let deletedBlog = await Blogmodel.findOneAndUpdate({ _id: blog_Id },
+            { $set: { isDeleted: true, deletedAt: new Date() } }, { new: true })
+        //Sending the Deleted response after updating isDeleted : true
+        return res.status(200).send({ status: true, msg: "Blog deleted succesfully" })
+    }
+    catch (err) {
+        console.log("This is the error :", err.message)
+        return res.status(500).send({ status: false, msg: " Server Error", error: err.message })
+    }
+}
+
 module.exports.createblog=createblog
 module.exports.blogsUpdate=blogsUpdate
+module.exports.deleted=deleted
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,7 @@ const Blogcontroller=require('../controller.js/blogcontroller')
 router.post('/authors',authorController.createAuthor)
 router.post('/blogs',Blogcontroller.createblog)
 router.put('/blogs/:blogid',Blogcontroller.blogsUpdate)
+router.delete("/blogs/:blogId",Blogcontroller.deleted)
 
 module.exports = router;
 // adding this comment for no reason
