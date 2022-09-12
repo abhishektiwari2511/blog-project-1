09a8f56c5c4be98f4e4458e9f3287c5513ebf5f4
Avantica123 <dattalandge001@gmail.com>
2022-09-12 11:37:48 +0530

    final

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ mongoose.connect("mongodb+srv://Chanchal25-DB:gFTcvqSDyVwmFSO9@cluster0.ypi01as.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', route);
-app.listen(process.env.PORT || 5000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 5000))
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
 
 
